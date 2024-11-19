73e43c051b95c54db0c27d54a9ac99c2f2ca961f
jhdelar <98446060+jhdelar@users.noreply.github.com>
2024-11-18 22:10:19 -0500

    road_v0.2.0-002

---
+++ dst/index.js
@@ -0,0 +1,7 @@
+import './css/t3s-util.css';
+import './css/t3s-ani.css';
+import './css/t3s-comp.css';
+
+export const t3sUtil = 't3s-util';
+export const t3sAni = 't3s-ani';
+export const t3sComp = 't3s-comp';
