ee8e86c922e0e6b874b8f04b493ea80b77fe47be
jhdelar <98446060+jhdelar@users.noreply.github.com>
2025-05-13 17:47:41 -0400

    road_v0.2.0-017
    - Added CSS string export.

--- dst/index.js
+++ dst/index.js
@@ -1,9 +1,21 @@
-import './css/t3s-base.css';
-import './css/t3s-util.css';
-import './css/t3s-ani.css';
-import './css/t3s-comp.css';
+// t3s.js
+import base from './css/t3s-base.css';
+import util from './css/t3s-util.css';
+import ani from './css/t3s-ani.css';
+import comp from './css/t3s-comp.css';
 
+// String identifiers for use elsewhere if needed
 export const t3sBase = 't3s-base';
 export const t3sUtil = 't3s-util';
 export const t3sAni = 't3s-ani';
 export const t3sComp = 't3s-comp';
+
+export const t3s = [
+    't3s-base',
+    't3s-util',
+    't3s-ani',
+    't3s-comp',
+]
+
+// Full combined CSS string
+export const t3sString = `${base}\n${util}\n${ani}\n${comp}`;
